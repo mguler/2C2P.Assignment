0172b7c8d1dd3d6997ec64827fd5dab0e27c34b0
murat.guler@airclinic.com.tr <murat.guler@airclinic.com.tr>
2021-05-13 14:19:49 +0300

    XSD schema validation implementation was added with tests

---
+++ 2c2p.Assignment.Validation/TransactionXmlXsdSchemaValidationConfigurationImpl.cs
@@ -0,0 +1,38 @@
+﻿using _2c2p.Assignment.Data.Model;
+using _2c2p.Assignment.Tools.Abstraction.Mapping;
+using _2c2p.Assignment.Tools.Abstraction.Validation;
+using _2c2p.Assignment.Tools.Impl.Validation;
+using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace _2c2p.Assignment.Validation
+{
+    public class TransactionXmlXsdSchemaValidationConfigurationImpl : IValidationConfiguration
+    {
+        public void Configure(IValidationServiceProvider validationServiceProvider)
+        {
+            validationServiceProvider.RegisterByKey("text/xml", new Func<string, IValidationResult>((input) =>
+            {
+                var transactions = XDocument.Parse(input);
+                var validationResult = new ValidationResult();
+
+                XmlSchemaSet schema = new XmlSchemaSet();
+                schema.Add("", "TransactionsXsd.xsd");
+                transactions.Validate(schema, (sender, e) =>
+                {
+                    XmlSeverityType type = XmlSeverityType.Warning;
+                    if (Enum.TryParse<XmlSeverityType>("Error", out type))
+                    {
+                        if (type == XmlSeverityType.Error) 
+                        {
+                            validationResult.IsValid = false;
+                            validationResult.Messages.Add(e.Message);
+                        }
+                    }
+                });
+                return validationResult;
+            }));
+        }
+    }
+}
---
+++ 2c2p.Assignment.WebApp.Tests/Validation/TransactionXmlXsdSchemaValidationConfigurationImplTests.cs
@@ -0,0 +1,86 @@
+﻿using _2c2p.Assignment.Tools.Abstraction.Validation;
+using _2c2p.Assignment.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace _2c2p.Assignment.WebApp.Tests.Bussiness
+{
+    [TestClass]
+
+    public class TransactionXmlXsdSchemaValidationConfigurationImplTests
+    {
+        private readonly string wrongxml = @"<Transactions>
+                        <Transaction id=""Inv000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"">
+                            <TransactionDate>T H I S  N O T  A  D A T E - T I M E  V A L U E</TransactionDate>
+                            <PaymentDetails>
+                                <Amount>A M O U N T  F I E L D  I S  A L S O  W R O N G!</Amount>
+                                <CurrencyCode> W R O N G  F I E L D </CurrencyCode>
+                            </PaymentDetails>
+                            <Status>D O N A  D O N A  D O N A  D O N A  D O!</Status>
+                        </Transaction>
+                        <Transaction id=""Inv00002"">
+                        <TransactionDate>2019-01-24T16:09:15</TransactionDate>
+                        <PaymentDetails>
+                            <Amount>10000.00</Amount>
+                            <CurrencyCode>EUR</CurrencyCode>
+                        </PaymentDetails>
+                        <Status> A N O T H E R  W R O N G  F I E L D </Status>
+                        </Transaction>
+                    </Transactions>";
+        private readonly string xml = @"<Transactions>
+                        <Transaction id=""Inv00001"">
+                            <TransactionDate>2019-01-23T13:45:10</TransactionDate>
+                            <PaymentDetails>
+                                <Amount>200.00</Amount>
+                                <CurrencyCode>USD</CurrencyCode>
+                            </PaymentDetails>
+                            <Status>Done</Status>
+                        </Transaction>
+                        <Transaction id=""Inv00002"">
+                        <TransactionDate>2019-01-24T16:09:15</TransactionDate>
+                        <PaymentDetails>
+                            <Amount>10000.00</Amount>
+                            <CurrencyCode>EUR</CurrencyCode>
+                        </PaymentDetails>
+                        <Status>Rejected</Status>
+                        </Transaction>
+                    </Transactions>";
+
+        [TestMethod]
+        public void ShouldReturnValidationErrors()
+        {
+
+            var validationServiceMockConfig = new Mock<IValidationServiceProvider>();
+            var validationConfiguration = new TransactionXmlXsdSchemaValidationConfigurationImpl();
+
+            Func<string, IValidationResult> validator = null;
+
+            validationServiceMockConfig.Setup(mockSetup => mockSetup.RegisterByKey(It.IsAny<string>(), It.IsAny<Func<string, IValidationResult>>()))
+               .Callback<string, Func<string, IValidationResult>>((name, func) => validator = func);
+
+            validationConfiguration.Configure(validationServiceMockConfig.Object);
+
+            var result = validator.DynamicInvoke(wrongxml) as IValidationResult;
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldPassTheValidation()
+        {
+
+            var validationServiceMockConfig = new Mock<IValidationServiceProvider>();
+            var validationConfiguration = new TransactionXmlXsdSchemaValidationConfigurationImpl();
+
+            Func<string, IValidationResult> validator = null;
+
+            validationServiceMockConfig.Setup(mockSetup => mockSetup.RegisterByKey(It.IsAny<string>(), It.IsAny<Func<string, IValidationResult>>()))
+               .Callback<string, Func<string, IValidationResult>>((name, func) => validator = func);
+
+            validationConfiguration.Configure(validationServiceMockConfig.Object);
+
+            var result = validator.DynamicInvoke(xml) as IValidationResult;
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
