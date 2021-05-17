aecd0dde55821d7b3186d37d6b0a4061beee0ae9
murat.guler@airclinic.com.tr <murat.guler@airclinic.com.tr>
2021-05-17 18:12:10 +0300

    xsd validation was added

---
+++ 2c2p.Assignment.Model.Mapping/XmlToTransactionDeserializerMappingConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using _2c2p.Assignment.Tools.Abstraction.Mapping;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Transactions;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace _2c2p.Assignment.Model.Mapping
+{
+    public class XmlToTransactionDeserializerMappingConfiguration : IMappingConfiguration
+    {
+        public void Configure(IMappingServiceProvider mappingServiceProvider)
+        {
+            mappingServiceProvider.RegisterByKey("text/xml", new Func<string, List<Transaction>>((input) =>
+            {
+                var result = new List<Transaction>();
+                var xmlDeserializer = new XmlSerializer(typeof(List<Transaction>));
+
+                var buffer = Encoding.UTF8.GetBytes(input);
+                var memoryStream = new MemoryStream(buffer);
+                result = (List<Transaction>)xmlDeserializer.Deserialize(memoryStream);
+
+                return result;
+            }));
+        }
+    }
+}
